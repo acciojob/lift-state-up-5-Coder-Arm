9bd316272d05a4181834925b200b84a537c50703
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-08-15 14:22:16 +0530

    pushing

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [isLoggedIn,setIsLoggedIn] = useState(false);
+
+function formHandler(e){
+   e.preventDefault();
+   setIsLoggedIn(true)
+}
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="parent">
+      <h1>Parent Component</h1>
+        <form onSubmit={formHandler} style={isLoggedIn === true?{display : "none"}:{display: 'unset'}}>
+         <label htmlFor="username">Username:
+         <input type="text" id="username"/>
+         </label>
+         <label htmlFor="pass">Password: 
+         <input type="password" id="pass"/>
+         </label>
+         <button>Login</button>
+        </form>
+        {isLoggedIn && <p>You are logged in!</p>}
     </div>
   )
 }
